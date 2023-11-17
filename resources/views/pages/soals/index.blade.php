edea2bb457a6c69a822cb6e6759fb94241234329
bahri <saiful.bahri.tl@gmail.com>
2023-11-18 05:57:10 +0700

    for tugas

--- resources/views/pages/soals/index.blade.php
+++ resources/views/pages/soals/index.blade.php
@@ -11,7 +11,7 @@
     <div class="main-content">
         <section class="section">
             <div class="section-header">
-                <h1>Bank Soal</h1>
+                <h1>Bank Soal - Tugas 2 Saiful Bahri</h1>
                 <div class="section-header-button">
                     <a href="{{ route('soal.create') }}" class="btn btn-primary">Add New</a>
                 </div>
